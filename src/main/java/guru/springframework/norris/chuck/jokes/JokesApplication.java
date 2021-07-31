81332dfaf786a9330f029e0809619e01fd0c95cb
Chris James <chrisdjames@btinternet.com>
2021-07-31 19:43:48 +0100

    Initial commit

---
+++ src/main/java/guru/springframework/norris/chuck/jokes/JokesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.norris.chuck.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/norris/chuck/jokes/JokesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.norris.chuck.jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
